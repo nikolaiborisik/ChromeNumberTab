0acf562844a86ca0b5c9a831ac6190a9b595f479
Nikolaj Borisik <n1k0lajby@gmail.com>
2012-12-30 18:43:00 +0300

    Add index 9 to last chrome tab

--- api.js
+++ api.js
@@ -1,22 +1,22 @@
-(function(){
-    var SHARP_STYLE  = '1',
+(function () {
+    var SHARP_STYLE = '1',
         SQUARE_STYLE = '2',
         PARENTHESES_STYLE = '3';
 
     window.ChromeNumberTabsApi = {
-        refreshTabsNumber : function () {
+        refreshTabsNumber:function () {
             chrome.tabs.getAllInWindow(null, function (tabs) {
 
                 for (var i = 0; i < tabs.length; i++) {
                     var t = tabs[i],
                         title = t.title,
-                        isShowIndex = t.index < 9,
-                        index = t.index + 1;
+                        isShowIndex = t.index < 8 || i === (tabs.length -1),
+                        index = i === (tabs.length -1) && t.index >= 8 ?  9 : t.index + 1;
                     title = title.replace(/^#\d/, "")
-                                 .replace(/^\[\d\]/, "")
-                                 .replace(/^\(\d\)/, "");
+                        .replace(/^\[\d\]/, "")
+                        .replace(/^\(\d\)/, "");
 
-                    switch (ChromeNumberTabsApi.getNumberStyle()){
+                    switch (ChromeNumberTabsApi.getNumberStyle()) {
                         case SHARP_STYLE :
                             title = isShowIndex ? "#" + index + " " + title : title;
                             break;
@@ -33,11 +33,11 @@
             });
         },
 
-        getNumberStyle : function(){
+        getNumberStyle:function () {
             return localStorage.getItem('ChromeNumberTabs_style') || SHARP_STYLE;
         },
 
-        setNumberStyle : function(style){
+        setNumberStyle:function (style) {
             return localStorage.setItem('ChromeNumberTabs_style', style);
         }
     };
